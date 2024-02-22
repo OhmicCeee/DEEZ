11dd8e2df39e051f7a72ce7078079203f21c6911
John Richards <jjr33@students.uwf.edu>
2024-02-22 02:54:17 -0500

    player class template and button changes shown

---
+++ player.js
@@ -0,0 +1,9 @@
+// JavaScript source code
+var gameData = {
+    Nut: 0,
+    GodPowers: 10000, //maybe god power thing? so like what do i even need a player class for? storing data ig????????????
+    Money: 0, //not sure if should track with player or money class
+    NPps: 0,
+    GPps: 0, //might be good to track these with player? or in other ones? ^^ too
+    Mps: 0
+};
