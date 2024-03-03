1ec6f5a75b24de7f05c537a129e38694a908a4e8
tdawgmillionaire <thetrevormonreal@gmail.com>
2024-03-03 15:40:03 -0800

    Nut Button Implementation

--- format.js
+++ format.js
@@ -35,7 +35,7 @@ function openTab(evt, tabName) {
 
 // Dark Mode Option
 
-const btn = document.querySelector(".button");
+const btn = document.querySelector(".toggle-button");
 const prefersDarkScheme = window.matchMedia("(prefers-color-scheme: dark)");
 
 const currentTheme = localStorage.getItem("theme");
